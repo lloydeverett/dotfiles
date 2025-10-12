5dcbb9f4ef34c3e2d203d47c36ce52518d95bf9b
Lloyd Everett <lloydeverett@gmail.com>
2025-10-12 07:18:06 +0200

    change quotes

--- .config/nvim/lua/treectl/highlights.lua
+++ .config/nvim/lua/treectl/highlights.lua
@@ -15,18 +15,18 @@ M.TreeModFs = "treectl_TreeModFs"
 M.TreeModOther = "treectl_TreeModOther"
 
 function M.configure()
-    vim.cmd('highlight link treectl_Comment Comment')
-    vim.cmd('highlight link treectl_Hidden Comment')
-    vim.cmd('highlight link treectl_Directory Directory')
-    vim.cmd('highlight link treectl_ErrorMsg ErrorMsg')
-    vim.cmd('highlight link treectl_Number Number')
-    vim.cmd('highlight link treectl_Debug SpecialChar')
-    vim.cmd('highlight link treectl_IndicatorActive SpecialChar')
-    vim.cmd('highlight link treectl_IndicatorInactive LineNr')
-    vim.cmd('highlight link treectl_TreeModBuiltins markdownH1')
-    vim.cmd('highlight link treectl_TreeModNvim markdownH5')
-    vim.cmd('highlight link treectl_TreeModFs markdownH4')
-    vim.cmd('highlight link treectl_TreeModOther markdownH2')
+    vim.cmd("highlight link treectl_Comment Comment")
+    vim.cmd("highlight link treectl_Hidden Comment")
+    vim.cmd("highlight link treectl_Directory Directory")
+    vim.cmd("highlight link treectl_ErrorMsg ErrorMsg")
+    vim.cmd("highlight link treectl_Number Number")
+    vim.cmd("highlight link treectl_Debug SpecialChar")
+    vim.cmd("highlight link treectl_IndicatorActive SpecialChar")
+    vim.cmd("highlight link treectl_IndicatorInactive LineNr")
+    vim.cmd("highlight link treectl_TreeModBuiltins markdownH1")
+    vim.cmd("highlight link treectl_TreeModNvim markdownH5")
+    vim.cmd("highlight link treectl_TreeModFs markdownH4")
+    vim.cmd("highlight link treectl_TreeModOther markdownH2")
 end
 
 return M
