486fafcfd562a97c16419b18d4ab2d783998e5d9
Lloyd Everett <lloydeverett@gmail.com>
2025-10-29 06:20:54 +0200

    remove conceal augroup

--- .config/nvim/lua/config/conceal.lua
+++ .config/nvim/lua/config/conceal.lua
@@ -1,6 +1,4 @@
 
-local augroup = vim.api.nvim_create_augroup('markdown', {})
-
 local function apply_custom_conceal()
         vim.api.nvim_set_hl(0, 'Conceal', { bg = 'NONE', fg = vim.g['terminal_color_6'] })
         vim.api.nvim_set_hl(0, 'todoCheckbox', { link = 'Todo' })
@@ -18,12 +16,10 @@ end
 
 vim.api.nvim_create_autocmd('BufEnter', {
     pattern = '*.md',
-    group = augroup,
     callback = apply_custom_conceal,
 })
 
 vim.api.nvim_create_autocmd('ColorScheme', {
-    group = augroup,
     callback = apply_custom_conceal,
 })
 
