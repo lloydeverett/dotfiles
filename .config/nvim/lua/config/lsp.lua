0a636b6b23a6f611c76610878cbfe0398756cc13
Lloyd Everett <lloydeverett@gmail.com>
2025-10-31 06:49:39 +0200

    code formatting

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -18,8 +18,8 @@ vim.diagnostic.config({
 vim.api.nvim_create_autocmd('LspAttach', {
     desc = 'LSP Actions',
     callback = function(_)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap = true, silent = true})
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {noremap = true, silent = true})
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, silent = true })
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })
     end,
 })
 
