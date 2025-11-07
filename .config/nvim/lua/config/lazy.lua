40d998f04a327cdafb21811a27ca85152df215f5
Lloyd Everett <lloydeverett@gmail.com>
2025-11-07 09:03:38 +0200

    add zenbones color schemes and remove harpoon

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -86,6 +86,9 @@ require("lazy").setup({
       { 'jpwol/thorn.nvim',
            opts = {}
       },
+      { "zenbones-theme/zenbones.nvim",
+           dependencies = "rktjmp/lush.nvim",
+      },
 
       -- plugins --------------------------------------------------------------------------------------------------
       { 'folke/snacks.nvim',
@@ -277,23 +280,6 @@ require("lazy").setup({
                require("nerdy").setup(opts)
                vim.keymap.set('n', '<leader>sn', "<cmd>Nerdy<CR>")
            end,
-      },
-      { "ThePrimeagen/harpoon",
-           branch = "harpoon2",
-           dependencies = { "nvim-lua/plenary.nvim" },
-           opts = {},
-           config = function(_, opts)
-               local harpoon = require("harpoon")
-               harpoon:setup(opts)
-               vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-               vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-               vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-               vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-               vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-               vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-               -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-               -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-           end
       },
       { "folke/todo-comments.nvim",
            dependencies = { "nvim-lua/plenary.nvim" },
