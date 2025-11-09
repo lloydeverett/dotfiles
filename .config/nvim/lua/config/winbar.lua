845e83e7b8f4bf5cf620b7412535f4600bf8490a
Lloyd Everett <lloydeverett@gmail.com>
2025-11-09 14:15:27 +0200

    winbar fix

--- .config/nvim/lua/config/winbar.lua
+++ .config/nvim/lua/config/winbar.lua
@@ -8,13 +8,16 @@ vim.api.nvim_create_autocmd("FileType", {
         if year ~= nil and month ~= nil and day ~= nil then
             local timestamp = os.time({ year = year, month = month, day = day })
             vim.wo.winbar = "󰃶 " .. os.date("%A, %d %B %Y", timestamp)
+            return
         end
+
         year = nil; month = nil; day = nil
         local month_pattern = "/(20%d%d)%-(%d%d)%.md$"
         year, month = string.match(path, month_pattern)
         if year ~= nil and month ~= nil then
             local timestamp = os.time({ year = year, month = month, day = 1 })
             vim.wo.winbar = "󰃶 " .. os.date("%B %Y", timestamp)
+            return
         end
     end,
 })
